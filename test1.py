67f02d2a509e79fcd67c1136480e8cd1376b553c
熊晓览 <a1774432258@outlook.com>
2022-10-02 23:31:27 +0800

    test

---
+++ test1.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+from functools import reduce
+from math import sqrt
+class Person(object):
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    # 访问器 - getter方法
+    @property
+    def name(self):
+        return self._name
+
+    # 访问器 - getter方法
+    @property
+    def age(self):
+        return self._age
+
+    # 修改器 - setter方法
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
